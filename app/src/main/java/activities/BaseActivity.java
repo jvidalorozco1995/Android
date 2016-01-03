95af1be8d9e62fa6ff887f7f0f5b949b195ae7b2
jvprestige <jorgevidal1995@hotmail.com>
2016-01-02 21:20:32 -0500

    Modificacion BaseActivity

--- app/src/main/java/activities/BaseActivity.java
+++ app/src/main/java/activities/BaseActivity.java
@@ -2,19 +2,36 @@ package activities;
 
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import com.buzz.yora.R;
 
 import infrastructure.YoraApplication;
 
 /**
  * Created by jorge on 01/11/2015.
  */
-public abstract class BaseActivity extends ActionBarActivity {
+public abstract class BaseActivity extends AppCompatActivity {
     protected YoraApplication application;
-
+    protected Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         application =(YoraApplication)getApplication();
+    }
+
+
+    @Override
+    public void setContentView(@LayoutRes int layoutResId) {
+        super.setContentView(layoutResId);
+
+        toolbar =(Toolbar)findViewById(R.id.include_login_header);
+        if(toolbar != null){
+            setSupportActionBar(toolbar);
+        }
     }
 }
--- app/src/main/java/activities/MainActivity.java
+++ app/src/main/java/activities/MainActivity.java
@@ -2,12 +2,17 @@ package activities;
 
 import android.os.Bundle;
 
+import com.buzz.yora.R;
+
 /**
  * Created by jorge on 01/11/2015.
  */
 public class MainActivity extends BaseAuthenticatedActivity {
+
     @Override
     protected void onYoraCreate(Bundle savedInstanceState) {
+     setContentView(R.layout.activity_main);
+     toolbar.setTitle("Inbox");
 
     }
 }
